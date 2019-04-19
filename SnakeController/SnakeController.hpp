e4acb831cdd74c044e0aa56ea7f2d1bf35e8c6f1
Bartosz Rodziewicz <bartoszka1996@gmail.com>
2019-04-19 16:56:00 +0200

    Get rid of basic types in snake controller

--- SnakeController/SnakeController.hpp
+++ SnakeController/SnakeController.hpp
@@ -50,14 +50,14 @@ private:
     void handleFoodResp(std::unique_ptr<Event>);
     void handlePauseInd(std::unique_ptr<Event>);
 
-    void updateSegmentsIfSuccessfullMove(int x, int y);
-    void addHeadSegment(int x, int y);
-    void removeTailSegmentIfNotScored(int x, int y);
+    void updateSegmentsIfSuccessfullMove(Position position);
+    void addHeadSegment(Position position);
+    void removeTailSegmentIfNotScored(Position position);
     void removeTailSegment();
 
-    void updateFoodPosition(int x, int y, std::function<void()> clearPolicy);
+    void updateFoodPosition(Position position, std::function<void()> clearPolicy);
     void sendClearOldFood();
-    void sendPlaceNewFood(int x, int y);
+    void sendPlaceNewFood(Position position);
 
     bool m_paused;
 };
